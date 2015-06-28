ee70e6c045c8c480be112e297fe272661cfb7a2e
Jacek Milewski <jacek.milewski.k@gmail.com>
2015-06-28 19:02:52 +0200

    String stream demo ready

--- src/main/java/com/looksok/StreamDemo.java
+++ src/main/java/com/looksok/StreamDemo.java
@@ -1,11 +1,31 @@
 package com.looksok;
 
 import java.lang.System;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
 
 class StreamDemo{
 
     public static void main(String[] args){
-        System.out.println("Hello, world!");
+
+        runStreamDemo();
+    }
+
+    private static void runStreamDemo() {
+        List<String> names = new ArrayList<String>();
+        names.add("John");
+        names.add("Jack");
+        names.add("Joe");
+        names.add("Michelle");
+
+        names.stream()
+            .filter(item -> item.length() > 3)
+            .filter((item -> item.startsWith("J")))
+            .sorted((item1, item2) -> (item2.length() - item1.length()))
+            .map(item -> "Hello, " + item)
+            .map(item -> item.toUpperCase() + "!")
+            .forEach(System.out::println);
     }
 
 }
