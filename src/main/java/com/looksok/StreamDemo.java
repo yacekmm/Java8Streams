1c5bdf3418df36d114800095e9034379163066bd
Jacek Milewski <jacek.milewski.k@gmail.com>
2015-06-28 19:16:58 +0200

    limit and collect

--- src/main/java/com/looksok/StreamDemo.java
+++ src/main/java/com/looksok/StreamDemo.java
@@ -2,8 +2,12 @@ package com.looksok;
 
 import java.lang.System;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Stream;
+
+import static java.util.Arrays.asList;
+import static java.util.stream.Collectors.toList;
 
 class StreamDemo{
 
@@ -19,13 +23,16 @@ class StreamDemo{
         names.add("Joe");
         names.add("Michelle");
 
-        names.stream()
-            .filter(item -> item.length() > 3)
-            .filter((item -> item.startsWith("J")))
-            .sorted((item1, item2) -> (item2.length() - item1.length()))
-            .map(item -> "Hello, " + item)
-            .map(item -> item.toUpperCase() + "!")
-            .forEach(System.out::println);
+        List<String> result = names.stream()
+                .filter(item -> item.length() > 3)
+                .filter((item -> item.startsWith("J")))
+                .sorted((item1, item2) -> (item2.length() - item1.length()))
+                .map(item -> "Hello, " + item)
+                .map(item -> item.toUpperCase() + "!")
+                .limit(1)
+                .collect(toList());
+
+        System.out.println(result);
     }
 
 }
