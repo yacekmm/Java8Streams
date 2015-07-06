cf411cd71353f52c369f1c91ab9b718723567a7c
jacek <jacek.milewski.k@gmail.com>
2015-07-06 11:32:18 +0200

    Streams with objects

--- src/main/java/com/looksok/StreamDemo.java
+++ src/main/java/com/looksok/StreamDemo.java
@@ -2,11 +2,9 @@ package com.looksok;
 
 import java.lang.System;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Stream;
 
-import static java.util.Arrays.asList;
 import static java.util.stream.Collectors.toList;
 
 class StreamDemo{
@@ -17,20 +15,76 @@ class StreamDemo{
     }
 
     private static void runStreamDemo() {
+//        namesDemo();
+
+        userDemo();
+    }
+
+
+
+
+
+
+    private static void namesDemo() {
         List<String> names = new ArrayList<String>();
         names.add("John");
         names.add("Jack");
         names.add("Joe");
         names.add("Michelle");
 
-        List<String> result = names.stream()
+        List<String> collect = names.stream()
                 .filter(item -> item.length() > 3)
-                .filter((item -> item.startsWith("J")))
-                .sorted((item1, item2) -> (item2.length() - item1.length()))
                 .map(item -> "Hello, " + item)
-                .map(item -> item.toUpperCase() + "!")
+                .sorted((item1, item2) -> (item2.length() - item1.length()))
                 .limit(1)
                 .collect(toList());
+
+        System.out.println(collect);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    private static void userDemo() {
+        List<User> names = new ArrayList<User>();
+        names.add(new User("John", 21));
+        names.add(new User("Jack", 13));
+        names.add(new User("Joe", 56));
+        names.add(new User("Michelle", 37));
+
+        Stream<User> result = names.stream()
+                .filter(item -> {
+                    return item.getAge() > 50;
+                });
+//                .collect(toList());
+
+//        result.
+//                forEach(System.out::println);
+
+        names.add(new User("Arnold", 38));
+
+        result.
+                forEach(System.out::println);
+
 
         System.out.println(result);
     }
---
+++ src/main/java/com/looksok/User.java
@@ -0,0 +1,31 @@
+package com.looksok;
+
+/**
+ * Created by jacek on 2015-07-06.
+ */
+public class User {
+
+    private String name;
+    private int age;
+
+    public User(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
